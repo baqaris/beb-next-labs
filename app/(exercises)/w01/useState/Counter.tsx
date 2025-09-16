04f3210fd169a4fc5eabd5e449e47093046cdd5e
baqaris <baqari.melashvili@gmail.com>
2025-09-17 00:19:21 +0400

    useState exercise
    count and input

---
+++ app/(exercises)/w01/useState/Counter.tsx
@@ -0,0 +1,26 @@
+// app\(exercises)\w01\useState\page.tsx
+
+"use client"
+
+import { useState } from "react"
+import styles from "./Counter.module.css";
+
+export default function Counter() {
+
+    const [count, setCount] = useState(0);
+
+    function increase() {
+        setCount(c => c + 1);
+        setCount(c => c + 1);
+        setCount(c => c + 1);
+    }
+
+    return (
+        <div className={styles.Container}>
+            <p className={styles.Counter}>{count}</p>
+            <button className={styles.button} onClick={()=> setCount(c => c + 1)}>+1</button>
+            <button   className={styles.button}  onClick={increase}>+3(functional)</button>
+            <button   className={styles.button} onClick={() => setCount(0)}>Reset</button>
+        </div>
+    );
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,7 @@
 
 "use client";
+import Counter from "./(exercises)/w01/useState/Counter";
+import NameForm from "./(exercises)/w01/useState/NameForm";
 import styles from "./page.module.css";
 import Button from "./src/components/Button/Button";
 import ProductCard from "./src/components/ProductCard/ProductCard";
@@ -15,6 +17,10 @@ export default function Home() {
         {products.map((p) => (
           <ProductCard key={p.id} item={p} />
         ))}
+        <div className={styles.Counter}>
+          <Counter/>
+        </div>
+        <NameForm/>
 
       </main>
       <footer className={styles.footer}>
